466ac66067b28de86a402c051b931a5133523efc
ShellsOfSilver <shellsofsilver13@gmail.com>
2020-09-12 22:16:17 +0300

    implemented enableClusterRecalculationOnMarkersUpdate

--- lib/src/marker_cluster_plugin.dart
+++ lib/src/marker_cluster_plugin.dart
@@ -4,10 +4,39 @@ import 'package:flutter_map_marker_cluster/src/marker_cluster_layer.dart';
 import 'package:flutter_map_marker_cluster/src/marker_cluster_layer_options.dart';
 
 class MarkerClusterPlugin extends MapPlugin {
+  MarkerClusterLayer _oldLayer;
+  int _markersHashCode;
+
+  /// Enable cluster recalculation on markers update. Default to false.
+  final bool enableClusterRecalculationOnMarkersUpdate;
+
+  MarkerClusterPlugin({
+    this.enableClusterRecalculationOnMarkersUpdate = false,
+  });
+
   @override
   Widget createLayer(
       LayerOptions options, MapState mapState, Stream<void> stream) {
-    return MarkerClusterLayer(options, mapState, stream);
+    if (!enableClusterRecalculationOnMarkersUpdate) {
+      return MarkerClusterLayer(options, mapState, stream);
+    }
+
+    MarkerClusterLayerOptions layerOptions = options;
+    int hashCode;
+    hashCode = layerOptions.markers.hashCode;
+
+    if (hashCode == _markersHashCode) {
+      if (_oldLayer != null) {
+        return _oldLayer;
+      } else {
+        _oldLayer = MarkerClusterLayer(options, mapState, stream);
+        return _oldLayer;
+      }
+    } else {
+      _markersHashCode = hashCode;
+      _oldLayer = MarkerClusterLayer(options, mapState, stream);
+      return _oldLayer;
+    }
   }
 
   @override
