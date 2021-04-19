6b72746084363a09095ab73a5757d76ce27ff8a8
Maximilian Kröll <39153701+Maz47@users.noreply.github.com>
2021-04-19 09:15:19 +0200

    Update flutter_map_marker_cluster.dart

--- lib/flutter_map_marker_cluster.dart
+++ lib/flutter_map_marker_cluster.dart
@@ -1,6 +1,7 @@
 library flutter_map_marker_cluster;
 
 export 'src/marker_cluster_layer.dart';
+export 'src/marker_cluster_layer_widget.dart';
 export 'src/marker_cluster_layer_options.dart';
 export 'src/marker_cluster_plugin.dart';
 
