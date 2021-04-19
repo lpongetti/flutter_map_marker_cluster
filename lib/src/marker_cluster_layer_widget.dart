de12c1ab5419ebadf06a63d985cc30155834ddec
Maximilian Kröll <39153701+Maz47@users.noreply.github.com>
2021-04-19 09:15:03 +0200

    Create marker_cluster_layer_widget.dart
    MarkerClusterLayerWidget added to support the new way to create layers for the flutter map (with widgets)

---
+++ lib/src/marker_cluster_layer_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+
+import '../flutter_map_marker_cluster.dart';
+
+class MarkerClusterLayerWidget extends StatelessWidget {
+  final MarkerClusterLayerOptions options;
+
+  MarkerClusterLayerWidget({Key key, @required this.options}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mapState = MapState.of(context);
+    return MarkerClusterLayer(options, mapState, mapState.onMoved);
+  }
+}
