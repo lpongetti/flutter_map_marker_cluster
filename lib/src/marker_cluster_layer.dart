cb5fb4ab342c23ce2ec07821c9fc80c985660b4c
Martin Berthier <martin.berthier.mb@gmail.com>
2022-02-08 13:56:08 +0100

    adding call to on marker tap method when hover is selected

--- lib/src/marker_cluster_layer.dart
+++ lib/src/marker_cluster_layer.dart
@@ -243,7 +243,7 @@ class _MarkerClusterLayerState extends State<MarkerClusterLayer>
       },
       child: GestureDetector(
         behavior: HitTestBehavior.opaque,
-        onTap: widget.options.popupOptions!.buildPopupOnHover ? () {} : _onMarkerTap(marker) as void Function()?,
+        onTap: widget.options.popupOptions!.buildPopupOnHover ? () {widget.options.onMarkerTap!;} : _onMarkerTap(marker) as void Function()?,
         child: widget.options.popupOptions!.buildPopupOnHover
           ? MouseRegion(
               onEnter: (_) => _onMarkerHoverEnter(marker),
