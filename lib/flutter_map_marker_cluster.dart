71ec20cd287cd79515a65ad7b813a3cdb92864e4
thkling <thomas.kling2@soprasteria.com>
2022-04-04 16:08:33 +0200

    Add export for marker_cluster_node.dart, so we can use it for the onClusterTap()

--- lib/flutter_map_marker_cluster.dart
+++ lib/flutter_map_marker_cluster.dart
@@ -1,8 +1,9 @@
 library flutter_map_marker_cluster;
 
-export 'src/marker_cluster_layer.dart';
-export 'src/marker_cluster_layer_widget.dart';
-export 'src/marker_cluster_layer_options.dart';
-export 'src/marker_cluster_plugin.dart';
-
 export 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
+
+export 'src/marker_cluster_layer.dart';
+export 'src/marker_cluster_layer_options.dart';
+export 'src/marker_cluster_layer_widget.dart';
+export 'src/marker_cluster_plugin.dart';
+export 'src/node/marker_cluster_node.dart';
