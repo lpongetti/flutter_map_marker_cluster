015d54618e925637e911666653da77150e2762bf
Your Name <you@example.com>
2024-07-24 22:13:07 +0200

    fix: set onDoubleTap on the marker widget to null as default and make it optional

--- lib/src/marker_widget.dart
+++ lib/src/marker_widget.dart
@@ -4,7 +4,7 @@ import 'package:flutter_map_marker_cluster/src/node/marker_node.dart';
 class MarkerWidget extends StatelessWidget {
   final MarkerNode marker;
   final VoidCallback onTap;
-  final VoidCallback onDoubleTap;
+  final VoidCallback? onDoubleTap;
   final Function(bool)? onHover;
   final bool buildOnHover;
   final bool markerChildBehavior;
@@ -12,7 +12,7 @@ class MarkerWidget extends StatelessWidget {
   MarkerWidget({
     required this.marker,
     required this.onTap,
-    required this.onDoubleTap,
+    this.onDoubleTap,
     required this.markerChildBehavior,
     this.onHover,
     this.buildOnHover = false,
